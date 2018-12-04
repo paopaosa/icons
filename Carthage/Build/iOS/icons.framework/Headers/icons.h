45d889a52fc9abb4a96d5543a4f815f2deb53ac5
Paopao sa <paopaosa@qq.com>
2018-12-04 17:15:31 +0800

    init

---
+++ Carthage/Build/iOS/icons.framework/Headers/icons.h
@@ -0,0 +1,19 @@
+//
+//  icons.h
+//  icons
+//
+//  Created by macpps on 2018/12/4.
+//  Copyright © 2018 ASR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for icons.
+FOUNDATION_EXPORT double iconsVersionNumber;
+
+//! Project version string for icons.
+FOUNDATION_EXPORT const unsigned char iconsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <icons/PublicHeader.h>
+
+
